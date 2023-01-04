55e11a066c761b522da94780396cfc0d9a999bba
zhh <zhanghui199111@163.com>
2023-01-04 21:16:11 +0800

    init proj

---
+++ src/main/java/org/example/HttpClientApplication.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * author: zhn4528
+ * create: 2023/1/4 21:13
+*/
+@SpringBootApplication
+public class HttpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientApplication.class, args);
+    }
+
+}
